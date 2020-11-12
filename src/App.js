4e6133190b66650fec9f1a22479ff397595d0a5c
ZeronP <nik4m.5hreyas@gmail.com>
2020-11-12 22:45:46 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoaded: false,
+    data: {},
+    error: null,
+  };
+  fetchRates() {
+    fetch("https://api.exchangeratesapi.io/latest?base=USD")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        this.setState({ isLoaded: true, data: res.rates });
+      });
+  }
+  componentDidMount() {
+    setInterval(() => this.fetchRates(), 1000);
+  }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>{error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <>
+          {Object.keys(this.state.data).map((key, index) => (
+            <p key={index}>
+              {key} {this.state.data[key]}
+            </p>
+          ))}
+        </>
+      );
+    }
+  }
 }
 
 export default App;
