05c61b6ab06ac5034bc525321ee0e73eaffbaab7
ZeronP <nik4m.5hreyas@gmail.com>
2020-11-17 21:57:39 +0530

    Updated lists

--- src/App.js
+++ src/App.js
@@ -1,42 +1,87 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
-
+import Convertor from "./Convertor";
 class App extends Component {
   state = {
     isLoaded: false,
-    data: {},
+    data: { rates: { "": { rate: "" } } },
     error: null,
+    history: {
+      USDINR: [0.0],
+      AUDUSD: [0.0],
+      EURGBP: [0.0],
+      EURUSD: [0.0],
+      GBPUSD: [0.0],
+      NZDUSD: [0.0],
+      USDAED: [0.0],
+    },
   };
+  maxval = 0;
+  maxkey = "USDINR";
   fetchRates() {
-    fetch("https://api.exchangeratesapi.io/latest?base=USD")
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        this.setState({ isLoaded: true, data: res.rates });
-      });
+    let urlLink =
+      "https://cors-anywhere.herokuapp.com/www.freeforexapi.com/api/live?pairs=USDINR,AUDUSD,EURGBP,EURUSD,GBPUSD,NZDUSD,USDAED";
+    setInterval(
+      60000,
+      fetch(urlLink)
+        .then((res) => res.json())
+        .then((res) => {
+          for (const key in res.rates) {
+            if (res.rates.hasOwnProperty(key)) {
+              const element = res.rates[key]["rate"];
+              if (this.state.history[key].length > 5) {
+                this.state.history[key].shift();
+              }
+              this.state.history[key].push(element);
+              if (this.maxval < element) {
+                this.maxval = element;
+                this.maxkey = key;
+              }
+            }
+          }
+        })
+        .then(() => {
+          this.setState({ isLoaded: true });
+        })
+    );
   }
   componentDidMount() {
-    setInterval(() => this.fetchRates(), 1000);
-  }
-  constructor(props) {
-    super(props);
+    try {
+      setInterval(async () => {
+        this.fetchRates();
+      }, 15000);
+    } catch (e) {
+      console.log(e);
+    }
   }
   render() {
-    const { error, isLoaded, items } = this.state;
+    const { error, isLoaded } = this.state;
     if (error) {
       return <div>{error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
-        <>
-          {Object.keys(this.state.data).map((key, index) => (
-            <p key={index}>
-              {key} {this.state.data[key]}
-            </p>
-          ))}
-        </>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              {this.maxkey} {this.maxval}
+            </div>
+          </div>
+          <div className="row">
+            {Object.keys(this.state.history).map((key, index) => (
+              <div className="col-sm-2" key={index}>
+                <p className="rateHead">{key}</p>
+                <ul className="ratesList">
+                  {this.state.history[key].map((e) => (
+                    <li className="ratesListItem">{e}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+          <div className="row">{/* <Convertor /> */}</div>
+        </div>
       );
     }
   }
---
+++ src/Convertor.jsx
@@ -0,0 +1,281 @@
+import React, { Component } from "react";
+class Convertor extends Component {
+  constructor(props) {
+    super(props);
+    this.handleFromChange = this.handleFromChange.bind(this);
+    this.handleToChange = this.handleToChange.bind(this);
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+    this.convert = this.convert.bind(this);
+  }
+  state = {
+    from: "",
+    to: "",
+    amount: "",
+    list: [
+      "AUD",
+      "USD",
+      "EUR",
+      "GBP",
+      "NZD",
+      "AED",
+      "AFN",
+      "ALL",
+      "AMD",
+      "ANG",
+      "AOA",
+      "ARS",
+      "ATS",
+      "AWG",
+      "AZM",
+      "AZN",
+      "BAM",
+      "BBD",
+      "BDT",
+      "BEF",
+      "BGN",
+      "BHD",
+      "BIF",
+      "BMD",
+      "BND",
+      "BOB",
+      "BRL",
+      "BSD",
+      "BTN",
+      "BWP",
+      "BYN",
+      "BYR",
+      "BZD",
+      "CAD",
+      "CDF",
+      "CHF",
+      "CLP",
+      "CNH",
+      "CNY",
+      "COP",
+      "CRC",
+      "CUC",
+      "CUP",
+      "CVE",
+      "CYP",
+      "CZK",
+      "DEM",
+      "DJF",
+      "DKK",
+      "DOP",
+      "DZD",
+      "EEK",
+      "EGP",
+      "ERN",
+      "ESP",
+      "ETB",
+      "FIM",
+      "FJD",
+      "FKP",
+      "FRF",
+      "GEL",
+      "GGP",
+      "GHC",
+      "GHS",
+      "GIP",
+      "GMD",
+      "GNF",
+      "GRD",
+      "GTQ",
+      "GYD",
+      "HKD",
+      "HNL",
+      "HRK",
+      "HTG",
+      "HUF",
+      "IDR",
+      "IEP",
+      "ILS",
+      "IMP",
+      "INR",
+      "IQD",
+      "IRR",
+      "ISK",
+      "ITL",
+      "JEP",
+      "JMD",
+      "JOD",
+      "JPY",
+      "KES",
+      "KGS",
+      "KHR",
+      "KMF",
+      "KPW",
+      "KRW",
+      "KWD",
+      "KYD",
+      "KZT",
+      "LAK",
+      "LBP",
+      "LKR",
+      "LRD",
+      "LSL",
+      "LTL",
+      "LUF",
+      "LVL",
+      "LYD",
+      "MAD",
+      "MDL",
+      "MGA",
+      "MGF",
+      "MKD",
+      "MMK",
+      "MNT",
+      "MOP",
+      "MRO",
+      "MRU",
+      "MTL",
+      "MUR",
+      "MVR",
+      "MWK",
+      "MXN",
+      "MYR",
+      "MZM",
+      "MZN",
+      "NAD",
+      "NGN",
+      "NIO",
+      "NLG",
+      "NOK",
+      "NPR",
+      "OMR",
+      "PAB",
+      "PEN",
+      "PGK",
+      "PHP",
+      "PKR",
+      "PLN",
+      "PTE",
+      "PYG",
+      "QAR",
+      "ROL",
+      "RON",
+      "RSD",
+      "RUB",
+      "RWF",
+      "SAR",
+      "SBD",
+      "SCR",
+      "SDD",
+      "SDG",
+      "SEK",
+      "SGD",
+      "SHP",
+      "SIT",
+      "SKK",
+      "SLL",
+      "SOS",
+      "SPL",
+      "SRD",
+      "SRG",
+      "STD",
+      "STN",
+      "SVC",
+      "SYP",
+      "SZL",
+      "THB",
+      "TJS",
+      "TMM",
+      "TMT",
+      "TND",
+      "TOP",
+      "TRL",
+      "TRY",
+      "TTD",
+      "TVD",
+      "TWD",
+      "TZS",
+      "UAH",
+      "UGX",
+      "UYU",
+      "UZS",
+      "VAL",
+      "VEB",
+      "VEF",
+      "VES",
+      "VND",
+      "VUV",
+      "WST",
+      "XAF",
+      "XAG",
+      "XAU",
+      "XBT",
+      "XCD",
+      "XDR",
+      "XOF",
+      "XPD",
+      "XPF",
+      "XPT",
+      "YER",
+      "ZAR",
+      "ZMK",
+      "ZMW",
+      "ZWD",
+    ],
+  };
+  handleAmountChange(event) {
+    this.setState({ amount: event.target.value });
+  }
+  handleFromChange(event) {
+    this.setState({ from: event.target.value });
+  }
+  handleToChange(event) {
+    this.setState({ to: event.target.value });
+  }
+  convert(event) {
+    event.preventDefault();
+    let conversion = this.state.from + this.state.to;
+    let urlLink =
+      "https://www.freeforexapi.com/api/live?pairs=" +
+      this.state.from +
+      this.state.to;
+    fetch(urlLink)
+      .then((res) => res.json())
+      .then((res) => {
+        let factor = res.rates[conversion]["rate"];
+        document.getElementById("result").innerHTML =
+          Number(factor) * Number(this.state.amount);
+      });
+  }
+  render() {
+    return (
+      <>
+        <form className="container">
+          <h5>Convertor</h5>
+          <select
+            name="from"
+            id="from"
+            onChange={this.handleFromChange}
+            required
+          >
+            {this.state.list.map((e) => (
+              <option key={e} value={e}>
+                e
+              </option>
+            ))}
+          </select>
+          To
+          <select name="to" id="to" onChange={this.handleToChange} required>
+            {this.state.list.map((e) => (
+              <option key={e} value={e}>
+                e
+              </option>
+            ))}
+          </select>
+          <input type="number" name="amount" id="amount" required />
+          <button className="btn btn-primary" onclick={this.convert}>
+            Convert
+          </button>
+          Result:
+          <div id="result"></div>
+        </form>
+      </>
+    );
+  }
+}
+
+export default Convertor;
