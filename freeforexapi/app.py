dcaf7110be20fdb595615dc797583f98b1b79064
Tanay Shirodkar <tanayshirodkar0@gmail.com>
2020-11-18 16:16:45 +0530

    add freeforex

---
+++ freeforexapi/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, jsonify
+import requests, json
+from datetime import datetime
+app = Flask(__name__)
+@app.route('/')
+def index():
+    x = requests.get("https://www.freeforexapi.com/api/live")
+    pairs = x.json().get('supportedPairs')
+    str = "https://www.freeforexapi.com/api/live?pairs="
+    for pair in pairs:
+        str+=pair+","
+    str = str[:-1]
+    resp = requests.get(str)
+    rates = resp.json().get('rates')
+    for rate in rates:
+        ts = rates[rate]['timestamp']
+        ts = int(ts)
+        rates[rate]['timestamp'] = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    return jsonify({"rates":rates})
+
+if __name__ == '__main__':
+    app.run()
