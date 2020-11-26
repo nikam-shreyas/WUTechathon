be6902e4d7f1a4fac8c9203f5265482991e2ffa6
Tanay Shirodkar <tanayshirodkar0@gmail.com>
2020-11-26 22:35:30 +0530

    Comment user model

--- backend/app.py
+++ backend/app.py
@@ -34,15 +34,15 @@ freeforexMax = freeforexMin = exchrateMax = exchrateMin = {}
 # def load_user(user_id):
 #     return User.query.get(int(user_id))
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(20), unique = True, nullable = False)
-    email = db.Column(db.String(120), unique = True, nullable = False)
-    fx_name = db.Column(db.String(20), unique = True, nullable = False)
-    password = db.Column(db.String(60), nullable = False)
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String(20), unique = True, nullable = False)
+#     email = db.Column(db.String(120), unique = True, nullable = False)
+#     fx_name = db.Column(db.String(20), unique = True, nullable = False)
+#     password = db.Column(db.String(60), nullable = False)
 
-    def __repr__(self):
-        return f"User('{self.name}', '{self.email}', '{self.fx_name}')"
+#     def __repr__(self):
+#         return f"User('{self.name}', '{self.email}', '{self.fx_name}')"
 
 
 
